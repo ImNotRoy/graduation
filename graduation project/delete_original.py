e39d0e53c0ec054e60fa6aada0a1fcf3c7b8a640
ImNotRoy <51410675+ImNotRoy@users.noreply.github.com>
2021-02-06 16:18:58 +0800

    Delete delete_original.py

--- graduation project/delete_original.py
+++
@@ -1,9 +0,0 @@
-import os
-
-base_dir='D:\毕设\graduation project\data'
-train_dir=os.path.join(base_dir,'train')
-validation_dir=os.path.join(base_dir,'validation')
-train_real_dir=os.path.join(train_dir,'real')
-train_synthesis_dir=os.path.join(train_dir,'synthesis')
-files=os.listdir(train_real_dir)
-print(files)
