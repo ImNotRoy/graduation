90d9d362b4976266ea58837a9a5c8fd0df0a8979
Long9 <1365987299@qq.com>
2021-02-09 00:00:57 +0800

    13

--- graduation project/extract_features.py
+++ graduation project/extract_features.py
@@ -1,9 +1,11 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import numpy as np
 import os.path
+from tqdm import tqdm
 from data import Dataset
 from CNN_extractor import CNN_Extractor
 
 seq_length=60
 data=Dataset(seq_length)
-
-print(data.train_data)
